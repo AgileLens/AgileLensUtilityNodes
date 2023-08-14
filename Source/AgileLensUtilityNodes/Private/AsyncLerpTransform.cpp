60c835b7fcf94725d3d928d033e60b678755619e
Nocxr <72276215+Nocxr@users.noreply.github.com>
2023-08-14 02:24:19 -0700

    added async lerp transform node

---
+++ Source/AgileLensUtilityNodes/Private/AsyncLerpTransform.cpp
@@ -0,0 +1,42 @@
+﻿// AsyncLerpTransform.cpp
+#include "AsyncLerpTransform.h"
+
+UAsyncLerpTransform::UAsyncLerpTransform()
+{
+	ElapsedTime = 0.0f;
+}
+
+UAsyncLerpTransform* UAsyncLerpTransform::LerpTransform(UObject* WorldContextObject, FTransform StartValue, FTransform EndValue, float Duration)
+{
+	UAsyncLerpTransform* LerpTask = NewObject<UAsyncLerpTransform>();
+	LerpTask->StartValue = StartValue;
+	LerpTask->EndValue = EndValue;
+	LerpTask->Duration = Duration;
+	LerpTask->TargetWorld = WorldContextObject->GetWorld();
+
+	LerpTask->TargetWorld->GetTimerManager().SetTimer(LerpTask->LerpTimerHandle, LerpTask, &UAsyncLerpTransform::HandleLerpTick, 0.01667f, true);
+
+	return LerpTask;
+}
+
+void UAsyncLerpTransform::HandleLerpTick()
+{
+	ElapsedTime += 0.01667f;
+
+	// Calculate lerp value for the transform
+	FVector LerpedLocation = FMath::Lerp(StartValue.GetLocation(), EndValue.GetLocation(), ElapsedTime / Duration);
+	FQuat LerpedRotation = FQuat::Slerp(StartValue.GetRotation(), EndValue.GetRotation(), ElapsedTime / Duration);
+	FVector LerpedScale = FMath::Lerp(StartValue.GetScale3D(), EndValue.GetScale3D(), ElapsedTime / Duration);
+	
+	FTransform CurrentTransform(LerpedRotation, LerpedLocation, LerpedScale);
+
+	// Check if we've finished
+	if (ElapsedTime >= Duration)
+	{
+		TargetWorld->GetTimerManager().ClearTimer(LerpTimerHandle);
+		OnLerpFinished.Broadcast(EndValue);
+		return;
+	}
+
+	OnLerpFinished.Broadcast(CurrentTransform);
+}
---
+++ Source/AgileLensUtilityNodes/Public/AsyncLerpTransform.h
@@ -0,0 +1,37 @@
+﻿// AsyncLerpTransform.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GameFramework/Actor.h"
+#include "AsyncLerpTransform.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTransformLerpFinished, FTransform, FinalValue);
+
+UCLASS(BlueprintType)
+class AGILELENSUTILITYNODES_API UAsyncLerpTransform : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UAsyncLerpTransform();
+
+	UFUNCTION(BlueprintCallable, Category = "Agile Lens", meta = (WorldContext = "WorldContextObject"))
+	static UAsyncLerpTransform* LerpTransform(UObject* WorldContextObject, FTransform StartValue, FTransform EndValue, float Duration);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnTransformLerpFinished OnLerpFinished;
+
+private:
+
+	UFUNCTION()
+	void HandleLerpTick();
+
+	FTransform StartValue;
+	FTransform EndValue;
+	float Duration;
+	float ElapsedTime;
+	FTimerHandle LerpTimerHandle;
+	TWeakObjectPtr<UWorld> TargetWorld;
+};
